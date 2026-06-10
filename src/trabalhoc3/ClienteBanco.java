c2605e114d7de6c790329523e02e4506570271b5
Gabriela Nascimento <gnascimento.cs@gmail.com>
2026-06-10 18:53:41 -0300

    Refactor ClienteBanco class and fix toString method

--- src/trabalhoc3/ClienteBanco.java
+++ src/trabalhoc3/ClienteBanco.java
@@ -6,8 +6,8 @@ public class ClienteBanco {
 	private int idade;
 	private char sexo;
 	private double saldo;
-	
-	
+
+
 	public ClienteBanco(String nome, String cpf, int idade, char sexo, double saldo) {
 		this.nome = nome;
 		this.cpf = cpf;
@@ -15,7 +15,7 @@ public class ClienteBanco {
 		this.sexo = Character.toUpperCase(sexo);
 		this.saldo = saldo;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
@@ -54,22 +54,22 @@ public class ClienteBanco {
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "===================================================\n"+
-				"Nome:" + nome + "/n" +
-				"CPF:" + cpf + "/n" +
-				"Idade:" + idade + "/n" +
-				"Sexo:" + sexo + "/n" +
-				//"Saldo: R$ " + String.format("%2.f", saldo) + "/n" +
+				"Nome:" + nome + "\n" +
+				"CPF:" + cpf + "\n" +
+				"Idade:" + idade + "\n" +
+				"Sexo:" + sexo + "\n" +
+				"Saldo: R$ " + String.format("%.2f", saldo) + "\n" +
 				"===================================================";
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 
 }
